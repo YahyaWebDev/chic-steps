48c45e7400499edd88d98ff90a67869415ae71de
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-01 13:58:56 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-// Define protected routes (optional)
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)']);
-
-export default clerkMiddleware((auth, req) => {
-  if (!isProtectedRoute(req)) return; // Skip auth for public routes
-  auth().protect(); // Enforce auth for protected routes
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
