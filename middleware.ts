020fa807c10a26019031c496267232758db64e99
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-01 16:30:53 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,12 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-const isProtectedRoute = createRouteMatcher([
-  '/',
-  '/profile(.*)'
-]);
-
-export default clerkMiddleware(async (auth, req) => {  // Note the async
-  if (isProtectedRoute(req)) {
-    const { userId } = await auth();  // Await the auth promise
-    if (!userId) {
-      return new Response('Unauthorized', { 
-        status: 307,
-        headers: { 
-          Location: 'https://chic-steps.vercel.app/natural-bluebird-84.clerk.accounts.dev' 
-        }
-      });
-    }
-  }
-});
+export default clerkMiddleware();
 
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 };
