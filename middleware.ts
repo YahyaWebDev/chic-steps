7cd024dab5e68bbc4d76fb2f057ef1fb88a46984
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-01 14:17:56 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+export default clerkMiddleware({
+  // Required for Vercel deployments
+  publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
