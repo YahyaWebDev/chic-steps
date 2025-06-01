10c754d7cb9ccb76e68cd3f7dbdfed0d80589e7f
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-01 14:21:25 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  module.exports = {
+  // Allow GLB files
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/files',
+          outputPath: 'static/files/',
+        },
+      },
+    });
+    return config;
+  },
+};
 };
 
 export default nextConfig;
