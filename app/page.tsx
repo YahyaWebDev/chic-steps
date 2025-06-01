04cfc24426db66d2ab75617d913efbc9b4d94485
Yahya Dev <152083102+YahyaWebDev@users.noreply.github.com>
2025-06-01 17:38:18 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,9 +57,9 @@ export default function Home(){
       <div className={`m-8 p-5 flex justify-center items-center flex-nowrap gap-5 flex-col`}>
         <Link href="/products">
         <button className={`${spaceGrotesk.className} absolute top-[80%] bg-gradient-to-tr from-lime-500 to-emerald-600 text-black text-center font-bold pr-2 pl-2 pt-3 pb-3 rounded-4xl hover:bg-gradient-to-br hover:from-sky-600 hover:to-blue-300 cursor-pointer outline-1 hover:text-blue-700 hover:outline-4 transition-all duration-1000 shadow-[0px_0px_15px_1px_#ffffff]`}>
-        Shop Now
+        <Link href="/products">Shop Now</Link>
         </button>
-        </Link>
+        
         <p className="flex flex-col justify-center items-center text-center flex-nowrap gap-2">
           <span>✨ Rotate, zoom, and customize in real-time</span>
           <span>🚀Free shipping on orders over $99</span>
